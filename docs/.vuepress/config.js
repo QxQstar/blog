5c97c4dfabb72521680e568c62a6c31214be6381
fds <fengdongsheng@lsh123.com>
2019-07-11 15:02:52 +0800

    微信小程序文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'Bella\'s documents',
+    description: '我见青山多妩媚，料青山见我应如是',
+    themeConfig: {
+        sidebarDepth:2,
+        sidebar: [
+            {
+                collapsable: false,
+                children: [
+                    '/'
+                ]
+            }
+        ]
+    }
+}
